305715de63357817f2dff96a123e5800f94e3ec8
valentina-RGB <valensc2545@gmail.com>
2025-05-28 15:37:40 -0500

    feat: :zap: add question types and improve layout components

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
-      <div className="h-screen flex flex-col items-center px-6 py-4  ring shadow-xl ring-gray-900/5 overflow-hidden ">
+      <div translate='no' className="h-screen flex flex-col items-center px-6 py-4  ring shadow-xl ring-gray-900/5 overflow-hidden ">
         <div className='relative h-full w-full  rounded-sm overflow-y-auto'>
           <div className='w-full overflow-y-auto  overflow-x-hidden h-full'>
             <Outlet />
--- front/src/common/widgets/admin/home_admin.widget.tsx
+++ front/src/common/widgets/admin/home_admin.widget.tsx
@@ -1,10 +1,15 @@
+import { MainLayout } from '@/common/widgets/MainLayout';
 
 export default function HomeAdmin() {
     return (
-      <div className=" h-full flex flex-col items-center p-2 justify-center  w-full">
-        <div className="bg-white  xl:w-5xl h-full">
- <p>m</p>
-        </div>
-      </div>
+        <MainLayout backgroundVariant="gradient">
+
+            <div className=" h-full flex flex-col items-center p-2 justify-center  w-full">
+                <div className="bg-white dark:bg-blue-400  xl:w-5xl h-full">
+                    <p>m</p>
+                </div>
+            </div>
+        </MainLayout>
+
     )
 }
