634dbef2f77c8395d3787dff7c44d6cf984fca1f
valentina-RGB <valensc2545@gmail.com>
2025-05-28 11:27:30 -0500

    feat: :zap: add dialog and card components with 404 page

---
+++ front/src/common/utils/404.tsx
@@ -0,0 +1,11 @@
+import notfount from '@/assets/notFound.svg'
+
+export const NotFound  = () => {
+    return (
+        <div>
+            <p>holaaa</p>
+            <img src={notfount} alt="" />
+        </div>
+    )
+}
+
--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -5,26 +5,29 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import RoleRoute from "./roleRoute";
 import { ROLES } from "@/common/utils/Roles";
 import { HomeClient } from "@/common/widgets/clients/home_client";
+import { NotFound } from "@/common/utils/404";
 
-export default function AuthRoutes  ()  {
+export default function AuthRoutes() {
     return (
-            <Router>
-                <Routes>
-                    {/* PRIVATE ROUTES  */}
+        <Router>
+            <Routes>
+                {/* PRIVATE ROUTES  */}
 
+                <Route path="/" element={<App />}>
+                    <Route path="/404" element={<NotFound />} />
                     <Route element={<RoleRoute allowedRoles={[ROLES.CLIENTE]} />}>
-                        <Route path="/home" element={<App />}>
-                            <Route index element={<HomeClient />} />
-                            
-                        </Route>
+                    <Route path="/client">
+                        <Route index element={<HomeClient />} />
                     </Route>
-                    <Route path="*" element={<Navigate to="/404" replace />} />
-                </Routes>
+                    </Route>
+                </Route>
+                <Route path="*" element={<Navigate to="/404" replace />} />
+            </Routes>
 
-            </Router>
+        </Router>
 
 
-  
-            )
-               
-                }
+
+    )
+
+}
