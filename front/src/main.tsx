19191529d15906d2df6b50cb67b9014c0ae4063c
valentina-RGB <valensc2545@gmail.com>
2025-05-28 10:57:13 -0500

    feat: :zap: implement role-based routing and dark mode toggle

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
+import AuthRoutes from './router';
 
 const queryClient = new QueryClient();
 
@@ -11,8 +12,7 @@ createRoot(document.getElementById('root')!).render(
     // <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <Toaster  />
-    <App />
-
+    <AuthRoutes />
 </QueryClientProvider>
   // </React.StrictMode>
 )
--- front/src/router/index.tsx
+++ front/src/router/index.tsx
@@ -1,1 +1,30 @@
 //aqui ira las rutas
+
+import App from "@/App";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import RoleRoute from "./roleRoute";
+import { ROLES } from "@/common/utils/Roles";
+import { HomeClient } from "@/common/widgets/clients/home_client";
+
+export default function AuthRoutes  ()  {
+    return (
+            <Router>
+                <Routes>
+                    {/* PRIVATE ROUTES  */}
+
+                    <Route element={<RoleRoute allowedRoles={[ROLES.CLIENTE]} />}>
+                        <Route path="/home" element={<App />}>
+                            <Route index element={<HomeClient />} />
+                            
+                        </Route>
+                    </Route>
+                    <Route path="*" element={<Navigate to="/404" replace />} />
+                </Routes>
+
+            </Router>
+
+
+  
+            )
+               
+                }
