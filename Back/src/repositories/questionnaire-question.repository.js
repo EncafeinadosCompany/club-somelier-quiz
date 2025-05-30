c52c25aab3076f24cc8d4b3be1fd3a9981612b65
Dm29015 <dmcorrea670@soy.sena.edu.co>
2025-05-30 16:38:11 -0500

    feat: refactor questionnaire question model and repository, enhance socket handling for admin and participant

--- Back/src/repositories/questionnaire-question.repository.js
+++ Back/src/repositories/questionnaire-question.repository.js
@@ -1,26 +1,29 @@
 const { QuestionnaireQuestion } = require('../models/questionnaire-question.model');
+const { Question } = require('../models/question.model');
 
 class QuestionnaireQuestionRepository {
-    async createMany(questionnaireQuestions, transaction) {
-        return await QuestionnaireQuestion.bulkCreate(questionnaireQuestions, {
-            transaction
-        });
-    }
+  async createMany(questionnaireQuestions, transaction) {
+    return await QuestionnaireQuestion.bulkCreate(questionnaireQuestions, {
+      transaction
+    });
+  }
 
-    async deleteByQuestionnaireandQuestion(questionnaireId, questionId) {
-        return await QuestionnaireQuestion.destroy({
-            where: {
-                questionnaire_id: questionnaireId,
-                question_id: questionId
-            },
-        });
-    }
+  async deleteByQuestionnaireandQuestion(questionnaireId, questionId) {
+    return await QuestionnaireQuestion.destroy({
+      where: {
+        questionnaire_id: questionnaireId,
+        question_id: questionId
+      },
+    });
+  }
 
-    async findByQuestionnaireId(questionnaireId) {
+  async findByQuestionnaireId(questionnaireId) {
     return await QuestionnaireQuestion.findAll({
       where: {
         questionnaire_id: questionnaireId,
       },
+      order: [['position', 'ASC']],
+      include: [{ model: Question, as: 'questions' }]
     });
   }
 
